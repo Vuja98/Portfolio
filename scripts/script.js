d8efb119611310791f97d86b82549887dfb1bbd6
Dejan Vujacic <vujacicdejan98@gmail.com>
2024-06-29 07:25:01 +0200

    Added function to the form

--- scripts/script.js
+++ scripts/script.js
@@ -144,3 +144,49 @@ close.addEventListener("click", () => {
   overlay.classList.remove("activate");
   modal.classList.remove("active");
 });
+
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+        setTimeout(() => {
+          setTimeout(() => {
+            modalWrapper.classList.remove("active");
+          }, 100);
+          overlay.classList.remove("activate");
+          modal.classList.remove("active");
+        }, 500);
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 3000);
+    });
+});
